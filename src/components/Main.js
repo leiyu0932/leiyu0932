c6e37e2d3a71f85bdd55311a61ea517bcf9d4e70
leiyu0932 <leiyu0932@163.com>
2016-10-18 11:17:11 +0800

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+//获取图片相关数据
+//let imageDatas = require('../data/imageDatas.json');
+//
+////利用自执行函数，将图片名信息转成图片URL路径信息
+//imageDatas = (function genImageURL(imageDatasArr){
+//    for(let i=0, j = imageDatasArr.length; i < j; i++){
+//        let singleImageData = imageDatasArr[i];
+//
+//        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+//    }
+//
+//    return imageDatasArr;
+//})(imageDatas);
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
